d0d80260c158c3364cdb9113be527d0e52ad552d
bindu prakash <bindup.bsc23@rvu.edu.in>
2024-02-19 13:34:30 +0530

    test

--- set03/problem04.c
+++ set03/problem04.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
 
-int input_number();
-int find_fibo(int n);
-void output(int n, int fibo);
-
-int main() {
-    int n;
-    printf("Enter the value of n: ");
-    n = input_number();
-    int fibo = find_fibo(n);
-    output(n, fibo);
-    return 0;
-}
-
-int input_number() {
+// Function to get input from the user
+int input() {
     int num;
+    printf("Enter a number: ");
     scanf("%d", &num);
     return num;
 }
 
+// Function to find the nth Fibonacci number
 int find_fibo(int n) {
     if (n <= 1)
         return n;
@@ -31,6 +21,18 @@ int find_fibo(int n) {
     return b;
 }
 
+// Function to output the result
 void output(int n, int fibo) {
     printf("The %dth Fibonacci number is: %d\n", n, fibo);
+}
+
+int main() {
+    int n;
+    // Get input from the user
+    n = input();
+    // Find the nth Fibonacci number
+    int fibo = find_fibo(n);
+    // Output the result
+    output(n, fibo);
+    return 0;
 }
